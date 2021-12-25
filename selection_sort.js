7e6ea2c7e07ff47559688d3393f58b1a161e0bb3
Syed Abdul Hannan <91002522+syedhannan@users.noreply.github.com>
2021-12-25 12:21:33 +0530

    Add files via upload

---
+++ selection_sort.js
@@ -0,0 +1,20 @@
+// Selection Sort function written in javascript
+function selectionSort(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    var lowest = i;
+    for (var j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[lowest]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) {
+      //SWAP!
+      var temp = arr[i];
+      arr[i] = arr[lowest];
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+
+console.log("Sorted Array", selectionSort([5, -1, 2, 3, 4, 0, -2, 1]));
